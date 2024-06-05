4e1d18f38fe533bfae23eab44c3c84f8014c1b03
V2RA <V2RAMail@gmail.com>
2024-06-05 17:20:50 +0100

    update

--- Services/plrJoin.js
+++ Services/plrJoin.js
@@ -9,16 +9,25 @@ JsMacros.on("PlayerJoin", JavaWrapper.methodToJava((event, context) => {
     // } else if (playerName === "Thomsk10") {
     // 
     // }
-    
+
     Chat.log("\u00a73\u00a7o" + playerName.toString() + " \u00a7b\u00a7ohas joined")
-    
+
     context.releaseLock();
 }));
 
 JsMacros.on("PlayerLeave", JavaWrapper.methodToJava((event, context) => {
     const playerName = event.player.getName()
-    
+
     Chat.log("\u00a73\u00a7o" + playerName.toString() + " \u00a7b\u00a7ohas left")
-    
+
     context.releaseLock();
 }));
+
+JsMacros.on("JoinServer", JavaWrapper.methodToJava((event, context) => {
+    Time.sleep(2500)
+    GlobalVars.putInt("coolDown", 0)
+}))
+
+JsMacros.on("Disconnect", JavaWrapper.methodToJava((event, context) => {
+    GlobalVars.putInt("coolDown", 1)
+}))
