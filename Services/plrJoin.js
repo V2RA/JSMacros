78ba7a7d9933cb6b05eba5d990286a2aff4ca8d8
V2RA <V2RAMail@gmail.com>
2024-06-05 17:23:14 +0100

    update

--- Services/plrJoin.js
+++ Services/plrJoin.js
@@ -1,14 +1,21 @@
-// Goes in services
-
 JsMacros.on("PlayerJoin", JavaWrapper.methodToJava((event, context) => {
     const playerName = event.player.getName()
 
-    // template for specific player messages
-    // if (playerName === "DrisBigMan") {
-    //    
-    // } else if (playerName === "Thomsk10") {
-    // 
-    // }
+    const pingForPlayers = [
+        "DrisBigMan",
+        "Thomsk10"
+    ]
+
+    if (GlobalVars.getInt("coolDown") === 1) {
+        if (pingForPlayers.includes(playerName)) {
+            Chat.actionbar("\u00a73\u00a7o" + playerName.toString() + " \u00a7b\u00a7ohas joined")
+            World.playSound("minecraft:entity.experience_orb.pickup", 1);
+        }
+
+        if (playerName === "DrisBigMan") {
+            Chat.say("baldy")
+        }
+    }
 
     Chat.log("\u00a73\u00a7o" + playerName.toString() + " \u00a7b\u00a7ohas joined")
 
