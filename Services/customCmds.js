b4bb24b866c8f4c28e5e76728b5c740ba79f8912
V2RA <V2RAMail@gmail.com>
2024-06-12 16:50:21 +0100

    actual commands

--- Services/customCmds.js
+++ Services/customCmds.js
@@ -50,4 +50,93 @@ JsMacros.on("SendMessage", JavaWrapper.methodToJava((event, context) => {
     if (cmd === "autism") {
         Chat.say("/nickname add &b&ldmu kid")
     }
+
+    if (cmd === "tl") {
+        if (args[0] !== undefined) {
+            let nick = nicks[args[0]]
+            let title = titles[args[0]]
+
+            if (args[1] === "true") {
+                Chat.log(`\u00A7bNickname would be: \u00A73${nick}`)
+                Chat.log(`\u00A7bTitle would be: \u00A73${title}`)
+            } else {
+                if (args[0] === "list") {
+                    for (let i = 0; i < titles.length; i++) {
+                        Chat.log(`\u00A7b${i}: ${titles[i]} ${nicks[i]}`)
+                    }
+                } else {
+                    Chat.say(`/nickname add ${nick}`);
+                    if (title == "") {
+                        Chat.say(`/set-title remove e`);
+                    } else {
+                        Chat.say(`/set-title add ${title}`);
+                    }
+                }
+            }
+        } else {
+            Chat.log(`\u00A7bTotal number of titles: ${titles.length}`)
+        }
+    }
+
+    if (cmd === "drop") {
+        const serviceMgr = JsMacros.getServiceManager()
+        const serviceName = "dropJunk"
+
+        const status = serviceMgr.status(serviceName)
+
+        if (GlobalVars.getInt("dropJunk") == 1) {
+            GlobalVars.putInt("dropJunk", 0)
+            Chat.actionbar("\u00A7bStopped \u00A73Dropping")
+        } else {
+            GlobalVars.putInt("dropJunk", 1)
+            Chat.actionbar("\u00A7bStarted \u00A73Dropping")
+        }
+
+    }
+
+    if (cmd === "dmg") {
+        if (GlobalVars.getInt("Damage") == 1) {
+            GlobalVars.putInt("Damage", 0)
+            Chat.actionbar("\u00A7bDamage Toggled: \u00A73Off")
+        } else {
+            GlobalVars.putInt("Damage", 1)
+            Chat.actionbar("\u00A7bDamage Toggled: \u00A73On")
+        }
+
+    }
+
+    if (cmd === "dnd") {
+        if (GlobalVars.getInt("DND") == 1) {
+            GlobalVars.putInt("DND", 0)
+            Chat.actionbar("\u00A7bDND Toggled: \u00A73Off")
+        } else {
+            GlobalVars.putInt("DND", 1)
+            Chat.actionbar("\u00A7bDND Toggled: \u00A73On")
+        }
+    }
+
+    if (cmd === "rld" || cmd === "reload") {
+        Chat.log(`\u00A7bReloading scripts`)
+        JsMacros.runScript("reloadServices.js")
+    }
+
+    if (cmd === "dbg") {
+        if (GlobalVars.getInt("debug") == 1) {
+            GlobalVars.putInt("debug", 0)
+            Chat.log(`\u00A7bDebug: \u00A73disabled`)
+        } else {
+            GlobalVars.putInt("debug", 1)
+            Chat.log(`\u00A7bDebug: \u00A73enabled`)
+        }
+    }
+
+    if (cmd === "afk") {
+        if (GlobalVars.getInt("AFK") == 1) {
+            GlobalVars.putInt("AFK", 0)
+            Chat.log(`\u00A7bAFK: \u00A73disabled`)
+        } else {
+            GlobalVars.putInt("AFK", 1)
+            Chat.log(`\u00A7bAFK: \u00A73enabled`)
+        }
+    }
 }))
