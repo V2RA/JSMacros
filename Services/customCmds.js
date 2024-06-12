c611bbc451affc16be85c83fad073b6b76b84a24
V2RA <V2RAMail@gmail.com>
2024-06-12 23:27:30 +0100

    gyatt

--- Services/customCmds.js
+++ Services/customCmds.js
@@ -18,14 +18,10 @@ function textHandler(text, symbol) {
     }
 }
 
-function c(t) {
-    return `\u00A7${t}`;
-}
-
 JsMacros.on("SendMessage", JavaWrapper.methodToJava((event, context) => {
     const plrs = World.getPlayers()
-    const cmd = event.message.toString().split(" ")[0].substring(1)
-    const args = event.message.toString().split(" ").slice(1)
+
+    let commandType;
 
     if (!event.message.toString().startsWith("/") && !event.message.toString().startsWith(".")) {
         return;
@@ -35,14 +31,14 @@ JsMacros.on("SendMessage", JavaWrapper.methodToJava((event, context) => {
 
     function exit() {
         if (commandType === ".") {
-            
+
         } else if (commandType === "/") {
             event.message = null
         }
     }
 
-    // exit()
-    // ^^^^^^ must go in every command
+    const cmd = event.message.toString().split(" ")[0].substring(1)
+    const args = event.message.toString().split(" ").slice(1)
 
     let playerList = []
 
@@ -52,114 +48,119 @@ JsMacros.on("SendMessage", JavaWrapper.methodToJava((event, context) => {
 
     function getPlayer(plrName) {
         for (player of playerList) {
-            if (player.toLowerCase().includes(plrName.toLowerCase())) {
+            if (plrName.toLowerCase() === "fat") {
+                return "TheCyberium"
+            } else if (plrName.toLowerCase() === "french") {
+                return "Conquerors_"
+            } else if (plrName.toLowerCase() === "twink") {
+                return "Jayson_json"
+            } else if (player.toLowerCase() === "bitch") {
+                return "wheezebob"
+            } else if (player.toLowerCase().includes(plrName.toLowerCase())) {
                 return player
             }
         }
     }
 
-    if (cmd === "autism") {
-        Chat.say("/nickname add &b&ldmu kid")
-    }
+    const cmds = {
+        "autism": () => {
+            Chat.say("/nickname add &b&ldmu kid")
+            exit()
+        },
 
-    if (cmd === "tl") {
-        if (args[0] !== undefined) {
-            let nick = nicks[args[0]]
-            let title = titles[args[0]]
+        "drop": () => {
+            const serviceMgr = JsMacros.getServiceManager()
+            const serviceName = "dropJunk"
 
-            if (args[1] === "true") {
-                Chat.log(`\u00A7bNickname would be: \u00A73${textHandler(nick, "\u00A7")}`)
-                Chat.log(`\u00A7bTitle would be: \u00A73${textHandler(title, "\u00A7")}`)
+            const status = serviceMgr.status(serviceName)
+
+            if (GlobalVars.getInt("dropJunk") == 1) {
+                GlobalVars.putInt("dropJunk", 0)
+                Chat.actionbar("\u00A7bStopped \u00A73Dropping")
             } else {
-                if (args[0] === "list") {
-                    for (let i = 0; i < titles.length; i++) {
-                        Chat.log(`\u00A7b${i}: ${textHandler(titles[i], "\u00A7")} \u00A7r| ${textHandler(nicks[i], "\u00A7")}`)
-                    }
+                GlobalVars.putInt("dropJunk", 1)
+                Chat.actionbar("\u00A7bStarted \u00A73Dropping")
+            }
+
+
+            exit()
+        },
+
+        "dmg": () => {
+            if (GlobalVars.getInt("Damage") == 1) {
+                GlobalVars.putInt("Damage", 0)
+                Chat.actionbar("\u00A7bDamage Toggled: \u00A73Off")
+            } else {
+                GlobalVars.putInt("Damage", 1)
+                Chat.actionbar("\u00A7bDamage Toggled: \u00A73On")
+            }
+
+            exit()
+        },
+
+        "dnd": () => {
+            if (GlobalVars.getInt("DND") == 1) {
+                GlobalVars.putInt("DND", 0)
+                Chat.actionbar("\u00A7bDND Toggled: \u00A73Off")
+            } else {
+                GlobalVars.putInt("DND", 1)
+                Chat.actionbar("\u00A7bDND Toggled: \u00A73On")
+            }
+
+            exit()
+        },
+
+        "tl": () => {
+            if (args[0] !== undefined) {
+                let nick = nicks[args[0]]
+                let title = titles[args[0]]
+
+                if (args[1] === "true") {
+                    Chat.log(`\u00A7bNickname would be: \u00A73${textHandler(nick, "\u00A7")}`)
+                    Chat.log(`\u00A7bTitle would be: \u00A73${textHandler(title, "\u00A7")}`)
                 } else {
-                    Chat.say(`/nickname add ${nick}`);
-                    if (title == "") {
-                        Chat.say(`/set-title remove e`);
+                    if (args[0] === "list") {
+                        for (let i = 0; i < titles.length; i++) {
+                            Chat.log(`\u00A7b${i}: ${textHandler(titles[i], "\u00A7")} \u00A7r| ${textHandler(nicks[i], "\u00A7")}`)
+                        }
                     } else {
-                        Chat.say(`/set-title add ${title}`);
+                        Chat.say(`/nickname add ${nick}`);
+                        if (title == "") {
+                            Chat.say(`/set-title remove e`);
+                        } else {
+                            Chat.say(`/set-title add ${title}`);
+                        }
                     }
                 }
+            } else {
+                Chat.log(`\u00A7bTotal number of titles: ${titles.length}`)
             }
-        } else {
-            Chat.log(`\u00A7bTotal number of titles: ${titles.length}`)
-        }
 
-        exit()
+            exit()
+        },
+
+        "rld": () => {
+            // alias reload
+            Chat.log(`\u00A7bReloading scripts`)
+            JsMacros.runScript("reloadServices.js")
+
+            exit()
+        },
+
+        "afk": () => {
+            if (GlobalVars.getInt("AFK") == 1) {
+                GlobalVars.putInt("AFK", 0)
+                Chat.actionbar(`\u00A7bAFK: \u00A73disabled`)
+            } else {
+                GlobalVars.putInt("AFK", 1)
+                Chat.actionbar(`\u00A7bAFK: \u00A73enabled`)
+            }
+
+            exit()
+        }
     }
 
-    if (cmd === "drop") {
-        const serviceMgr = JsMacros.getServiceManager()
-        const serviceName = "dropJunk"
-
-        const status = serviceMgr.status(serviceName)
-
-        if (GlobalVars.getInt("dropJunk") == 1) {
-            GlobalVars.putInt("dropJunk", 0)
-            Chat.actionbar("\u00A7bStopped \u00A73Dropping")
-        } else {
-            GlobalVars.putInt("dropJunk", 1)
-            Chat.actionbar("\u00A7bStarted \u00A73Dropping")
-        }
-
-        exit()
-    }
-
-    if (cmd === "dmg") {
-        if (GlobalVars.getInt("Damage") == 1) {
-            GlobalVars.putInt("Damage", 0)
-            Chat.actionbar("\u00A7bDamage Toggled: \u00A73Off")
-        } else {
-            GlobalVars.putInt("Damage", 1)
-            Chat.actionbar("\u00A7bDamage Toggled: \u00A73On")
-        }
-
-        exit()
-    }
-
-    if (cmd === "dnd") {
-        if (GlobalVars.getInt("DND") == 1) {
-            GlobalVars.putInt("DND", 0)
-            Chat.actionbar("\u00A7bDND Toggled: \u00A73Off")
-        } else {
-            GlobalVars.putInt("DND", 1)
-            Chat.actionbar("\u00A7bDND Toggled: \u00A73On")
-        }
-
-        exit()
-    }
-
-    if (cmd === "rld" || cmd === "reload") {
-        Chat.log(`\u00A7bReloading scripts`)
-        JsMacros.runScript("reloadServices.js")
-
-        exit()
-    }
-
-    if (cmd === "dbg") {
-        if (GlobalVars.getInt("debug") == 1) {
-            GlobalVars.putInt("debug", 0)
-            Chat.log(`\u00A7bDebug: \u00A73disabled`)
-        } else {
-            GlobalVars.putInt("debug", 1)
-            Chat.log(`\u00A7bDebug: \u00A73enabled`)
-        }
-
-        exit()
-    }
-
-    if (cmd === "afk") {
-        if (GlobalVars.getInt("AFK") == 1) {
-            GlobalVars.putInt("AFK", 0)
-            Chat.log(`\u00A7bAFK: \u00A73disabled`)
-        } else {
-            GlobalVars.putInt("AFK", 1)
-            Chat.log(`\u00A7bAFK: \u00A73enabled`)
-        }
-
-        exit()
+    if (cmd in cmds) {
+        cmds[cmd]()
     }
 }))
