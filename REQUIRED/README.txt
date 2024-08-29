be7101b285aea2990dbcba0f19c0017a72cce32b
V2RA <V2RAMail@gmail.com>
2024-08-29 20:35:18 +0100

    UPDATE

---
+++ REQUIRED/README.txt
@@ -0,0 +1,3 @@
+The Storage.json file needs to go in the same folder as the script files.
+
+This is usually {profile}/config/jsMacros/Macros/
--- Services/customCmds.js
+++ Services/customCmds.js
@@ -86,6 +86,116 @@ Chat.createCommandBuilder("afk")
         Chat.actionbar(`\u00A7bAFK Toggled: \u00A73${GlobalVars.getInt("AFK") == 1 ? "enabled" : "disabled"}`)
     })).register()
 */
+
+// --------======={ Toggles } ======-------- \\
+// --------======={ Misc } ======-------- \\
+
+Chat.createCommandBuilder("scriptsettings")
+    .wordArg("key")
+    .suggestMatching("titleaddcmd", "titleremovecmd", "nicknameaddcmd", "nicknameremovecmd")
+    .executes(JavaWrapper.methodToJava(c => {
+        const settingsFile = m.StorageFile("get")
+        const address = World.getCurrentServerAddress().toString();
+
+        if (settingsFile[address].Settings) {
+            if (settingsFile[address].Settings[c.getArg("key")]) {
+                Chat.log(`// ${c.getArg("key")} = ${settingsFile[address].Settings[c.getArg("key")]}`)
+                Chat.log(`\\\\ Use "{val}" to designate where the value should be positioned within the command.`)
+            } else Chat.log(`\u00A7c\u00A7n\u00A7o${c.getArg("key")}\u00A7c does not exist`);
+        } else Chat.log(`\u00A7cSettings file does not exist`);
+    }))
+    .greedyStringArg("value")
+    .executes(JavaWrapper.methodToJava(c => {
+        const settingsFile = m.StorageFile("get")
+        const address = World.getCurrentServerAddress().toString();
+
+        if (settingsFile[address].Settings) {
+            settingsFile[address].Settings[c.getArg("key")] = c.getArg("value")
+            m.StorageFile("save", settingsFile)
+            Chat.log(`// ${c.getArg("key")} = ${c.getArg("value")}`)
+        } else Chat.log(`\u00A7cSettings file does not exist`)
+    })).register();
+
+Chat.createCommandBuilder("titlelist")
+    .wordArg("key")
+    .suggestMatching("set", "list", "add", "remove")
+    .executes(JavaWrapper.methodToJava(c => {
+        const settingsFile = m.StorageFile("get")
+        const address = World.getCurrentServerAddress().toString();
+        if (settingsFile[address].Titles) {
+            if (c.getArg("key") == "list") {
+                if (settingsFile[address].Titles.length <= 0) return Chat.log(`\u00A7cNo titles exist`);
+
+                Chat.log(`\u00A7bTitles:`)
+                for (title of Object.keys(settingsFile[address].Titles)) {
+                    Chat.log(`${title} = ${m.textHandler(settingsFile[address].Titles[title].title, "\u00A7")} | ${m.textHandler(settingsFile[address].Titles[title].nickname, "\u00A7")}`)
+                }
+            } else return Chat.log(`\u00A7cInvalid arguments`)
+        } else {
+            Chat.log(`\u00A7cCreating titles list does not exist`)
+            settingsFile[address].Titles = []
+            m.StorageFile("save", settingsFile)
+        }
+    }))
+    .quotedStringArg("value/nickname")
+    .executes(JavaWrapper.methodToJava(c => {
+        const settingsFile = m.StorageFile("get")
+        const address = World.getCurrentServerAddress().toString();
+
+        if (settingsFile[address].Titles) {
+            if (c.getArg("key") == "set") {
+                if (!settingsFile[address].Settings.titleaddcmd) return Chat.log(`\u00A7cNo title add command set`);
+                if (!settingsFile[address].Settings.nicknameaddcmd) return Chat.log(`\u00A7cNo nickname add command set`);
+
+                if (settingsFile[address].Titles[c.getArg("value/nickname")].title == "") Chat.say(settingsFile[address].Settings.titleremovecmd);
+                else Chat.say(`${settingsFile[address].Settings.titleaddcmd.replaceAll("{val}", settingsFile[address].Titles[c.getArg("value/nickname")].title)}`);
+                Chat.say(`${settingsFile[address].Settings.nicknameaddcmd.replaceAll("{val}", settingsFile[address].Titles[c.getArg("value/nickname")].nickname)}`)
+            } else if (c.getArg("key") == "remove") {
+                if (settingsFile[address].Titles[c.getArg("value/nickname")]) {
+                    Chat.log(m.textHandler(`\u00A7bRemoved \u00A73${settingsFile[address].Titles[c.getArg("value/nickname")].title} | ${settingsFile[address].Titles[c.getArg("value/nickname")].nickname} \u00A7bfrom the list`, "\u00A7"));
+                    settingsFile[address].Titles.splice(c.getArg("value/nickname"), 1)
+                    m.StorageFile("save", settingsFile)
+                }
+            } else return Chat.log(`\u00A7cInvalid arguments`)
+        }
+    }))
+    .quotedStringArg("value2/title")
+    .executes(JavaWrapper.methodToJava(c => {
+        const settingsFile = m.StorageFile("get")
+        const address = World.getCurrentServerAddress().toString();
+
+        if (settingsFile[address].Titles) {
+            if (c.getArg("key") == "add") {
+                const int = (settingsFile[address].Titles.length || 0)
+                settingsFile[address].Titles[int] = { nickname: c.getArg("value/nickname"), title: c.getArg("value2/title") }
+                m.StorageFile("save", settingsFile)
+                Chat.log(m.textHandler(`\u00A7bAdded \u00A73${c.getArg("value2/title")} ${c.getArg("value/nickname")} \u00A7bto the list`, "\u00A7"));
+            }
+        }
+    })).register();
+
+// \\ --------======={ Misc } ======-------- // \\
+// --------======={ Quality Of Life } ======-------- \\
+
+Chat.createCommandBuilder("to")
+    .greedyStringArg("player")
+    .executes(JavaWrapper.methodToJava(c => {
+        Chat.say(`/tp @s ${getPlayer(c.getArg("player"))}`)
+    })).register();
+
+Chat.createCommandBuilder("bring")
+    .wordArg("player")
+
+    .executes(JavaWrapper.methodToJava(c => {
+        Chat.say(`/tp ${getPlayer(c.getArg("player"))} @s`)
+    }))
+
+    .intArg("distance")
+    .executes(JavaWrapper.methodToJava(c => {
+        Chat.say(`/tp ${getPlayer(c.getArg("player"))} ^ ^ ^${c.getArg("distance")}`)
+    })).register();
+
+// \\ --------======={ Quality Of Life } ======-------- // \\
 // --------======={ Gamemodes } ======-------- \\
 
 Chat.createCommandBuilder("gmc")
@@ -124,6 +234,7 @@ Chat.createCommandBuilder("gma")
         Chat.say(`/gamemode adventure ${getPlayer(c.getArg("player"))}`)
     })).register()
 
+// \\ --------======={ Gamemodes } ======-------- // \\
 /*
 // --------======={ Creating your own commands } ======-------- \\
 
